e6093def9349976f4b234fe942aecc757f14b5fc
Probainsa <49351633+Probainsa@users.noreply.github.com>
2019-04-20 15:07:30 +0100

    plan component

--- plan.component.ts
+++ plan.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {DietDay} from '../Shared/plan-detail.component';
 
 @Component({
   templateUrl: './plan.component.html',
   styleUrls: ['./plan.component.css']
 })
 export class PlanComponent implements OnInit {
+public selectedDay: number= 0;
+
+myPlan: DietDay[];
+
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  dayClicked (dayNumber:number): void {
+    this.selectedDay=dayNumber;
+  }
 }
